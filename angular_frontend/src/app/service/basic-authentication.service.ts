236ee769e48e19fc85a85b294c6289f08b3bbaab
way2anash <ansarianash.np@gmail.com>
2020-05-09 14:46:28 +0530

    initial commit

---
+++ angular_frontend/src/app/service/basic-authentication.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { API_URL } from '../app.constants';
+
+export const TOKEN = 'token';
+export const AUTHENTICATED_USER = 'authenticaterUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  executeJWTAuthenticationService(username, password) {
+    //let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+
+    return this.http.post<any>(`${API_URL}/authenticate`,{
+      username,
+      password
+    }).pipe(
+      map(
+        data => {
+          sessionStorage.setItem(AUTHENTICATED_USER, username);
+          sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
+
+          return data;
+        }
+      )
+    );
+  }
+
+
+  executeAuthenticationService(username, password) {
+    //let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    }
+
+    )
+
+    return this.http.get<AuthenticationBean>(`${API_URL}/basicauth`,
+    {headers}).pipe(
+      map(
+        data => {
+          sessionStorage.setItem(AUTHENTICATED_USER, username);
+          sessionStorage.setItem(TOKEN, basicAuthHeaderString);
+
+          return data;
+        }
+      )
+    );
+  }
+
+
+  // createBasicAuthenticationHttpHeader() {
+  //   let username = 'nepdroid'
+  //   let password = 'anash'
+  //   let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+  //   return basicAuthHeaderString;
+  // }
+
+  getAuthenticatedUser() {
+    return sessionStorage.getItem(AUTHENTICATED_USER) 
+  }
+
+  getAuthenticatedToken() {
+    if(this.getAuthenticatedUser())
+    return sessionStorage.getItem(TOKEN) 
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem(AUTHENTICATED_USER)
+    return !(user === null)
+  }
+
+  logout(){
+    sessionStorage.removeItem(AUTHENTICATED_USER)
+    sessionStorage.removeItem(TOKEN)
+  }
+}
+
+export class AuthenticationBean {
+  constructor(public message: string) {}
+}
---
+++ restful_backend/src/main/java/com/nepdroid/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.nepdroid.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
